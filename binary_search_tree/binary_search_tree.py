7766f368769bf5bf480967207b2fb051ba6c59c3
Alex <agordon1123@gmail.com>
2019-12-19 17:17:06 -0500

    completed in_order_print and dft_print

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,5 +1,5 @@
 import sys
-# sys.path.append('../queue_and_stack')
+sys.path.append('../queue_and_stack')
 from dll_queue import Queue
 from dll_stack import Stack
 
@@ -53,18 +53,25 @@ class BinarySearchTree:
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
+        cb(self.value)
         if self.right:
             self.right.for_each(cb)
         if self.left:
             self.left.for_each(cb)
-        cb(self.value)
 
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        if node.left:
+            self.in_order_print(node.left)
+        if node.right:
+            print(node.value) 
+            self.in_order_print(node.right)
+        else:
+            print(node.value)
+            return
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
@@ -74,7 +81,20 @@ class BinarySearchTree:
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        stack = Stack()
+        stack.push(node)
+
+        while stack.len() > 0:
+            root = stack.pop()
+            print(root.value)
+
+            if root.left:
+                stack.push(root.left)
+            if root.right:
+                stack.push(root.right)
+            
+            continue
+            
 
     # STRETCH Goals -------------------------
     # Note: Research may be required
